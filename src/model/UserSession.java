0d08254bfb0afcc647467b53f0df1cac198cc16b
Tindingu <thanhtin95123@gmail.com>
2025-05-28 20:22:06 +0700

    update session

---
+++ src/model/UserSession.java
@@ -0,0 +1,14 @@
+// File: model/UserSession.java (tạo mới)
+package model;
+
+public class UserSession {
+    private static String cccd;
+
+    public static String getCccd() {
+        return cccd;
+    }
+
+    public static void setCccd(String value) {
+        cccd = value;
+    }
+}
